8e9959373169b29fe422c1fdecda2ecaee49ef77
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2025-01-06 16:00:02 +0200

    new update

---
+++ home page/script.js
@@ -0,0 +1,8 @@
+// let run = "true";
+// let running;
+
+// do {
+//   running = prompt("running:"); // Simulate user input
+// } while (running !== correctPassword);
+
+// console.log("Login successful!");
